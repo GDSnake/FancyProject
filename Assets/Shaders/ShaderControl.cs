81c6a5eb8cecf73caa3b5119486a54fa7dbbaad5
GDSnake <gon.n.delgado@gmail.com>
2018-07-19 22:54:46 +0100

    Minor Changes

---
+++ Assets/Shaders/ShaderControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ShaderControl class simply sends the Time in seconds since beginning times 2 to the _Sum variable on the GradientLitShader, normalized from 0 to 2 and
+/// 6 times to the _Mult variable on the GradientLitShader, normalized from 0 to 2. 
+/// </summary>
+public class ShaderControl : MonoBehaviour
+{
+    
+    private float _sumValue;
+    private float _multValue;
+	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _multValue = Mathf.Cos(Time.time*6);
+	    _sumValue = Mathf.Sin(Time.time*2);
+	    foreach (var material in GetComponent<Renderer>().materials)
+	    {
+            material.SetFloat("_Sum", _sumValue + 1);
+            material.SetFloat("_Mult", _multValue + 1);
+        }
+        
+        
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,18 @@
+All art assets were taken from various unity tutorials.
+GradientLitShader was made on a previous Game Jam by me.
+
+CONTROLS:
+- WASD - Move player's ship
+- Space - Shoot
+- Tab - Lock target in view and change from various targets in view
+- X - Cancel lock on
+- H - Toggle help
+- R - Restart
+
+FEATURES:
+- Custom Shader that changes object color over time
+- Enemies attack when you enter in certain range
+
+Issues:
+- Sometimes the lockOn gets finicky when the target gets on the edge and/or you're shooting at him, it shouldn't happen but it does, don't know why,
+if that happens simply reset the lock (with 'X' and re-enable with Tab) and it work's fine.
